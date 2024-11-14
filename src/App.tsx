6a15990a8cb485154eaefb63fc29352e09e4166e
siltea <siltea2019@gmail.com>
2024-11-14 17:18:03 +0900

    Design: header 레이아웃 중간저장

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,15 @@ function App() {
       <Routes>
         <Route element={<CommonLayout />}>
           <Route path='/' element={<MainPage />} />
+          <Route path='/common' element={<Common />} />
+          <Route path='/estimation' element={'견적페이지 연결해주세요'} />
+          <Route path='/login' element={'login 페이지 연결해주세요'} />
+          <Route path='/mypage' element={'mypage 페이지 연결해주세요'} />
         </Route>
-        <Route path='/common' element={<Common />} />
+        <Route path='/chat' element={'채팅페이지 연결해주세요'} />
       </Routes>
     </>
   );
 }
 
 export default App;
-
-
