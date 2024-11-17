eac6f30f6186f0bbab8543c55391e4aa6ddfffba
SOOMIN <doda2031@naver.com>
2024-11-17 22:37:29 +0900

    Chore : env import를 위한 설정 변경

---
+++ src/api/sendAuthCodeAPI.tsx
@@ -0,0 +1,12 @@
+// import socialLoginAPI from './socialLoginAPI';
+// const redirectBaseURL = import.meta.env.VITE_REDIRECT_BASE_URI;
+
+// interface SocialLoginPayload {
+//   provider: 'naver' | 'kakao' | 'google';
+//   authCode: string;
+//   state?: string; // naver
+// }
+// export const sendAuthCodeAPI = async ({ provider, authCode, state }: SocialLoginPayload) => {
+//   const redirectURL = `${redirectBaseURL}`;
+//   const callbackURL = `${provider}/callback/`;
+// };
