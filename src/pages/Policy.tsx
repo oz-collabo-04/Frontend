d69d5a40b9361b47a45ffca0d98244e34ab1a46b
siltea <siltea2019@gmail.com>
2024-11-17 16:25:44 +0900

    Design : footer 레이아웃 1차완성 및 페이지링크수정

---
+++ src/pages/Policy.tsx
@@ -0,0 +1,16 @@
+// 복사안됨 ^^
+// 레알 노가다 뛰어야함
+// 이용약관 내용
+// https://www.sonewwedding.com/13022500-6206-80b4-892d-f7358230dc56
+// 개인정보처리방침
+// https://www.sonewwedding.com/13022500-6206-800e-b533-fefd6484fef8
+
+// 이용약관
+export const ConditionsOfUse = () => {
+  return <div>ConditionsOfUse</div>;
+};
+
+// 개인정보 처리방침
+export const PersonalInformation = () => {
+  return <div>PersonalInformation</div>;
+};
