1d63a9e3f4844fda378c8a14a806576fe78886fd
SOOMIN <doda2031@naver.com>
2024-11-20 13:33:11 +0900

    Feat : 네이버 로그인 및 AT 로컬스토리지 저장

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,9 @@
 interface ImportMetaEnv {
   readonly VITE_NAVER_CLIENT_ID: string; // 네이버 클라이언트 ID
   readonly VITE_REDIRECT_BASE_URL: string; // 리다이렉트 URI
+  readonly VITE_BACKEND_BASE_URL: string;
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_KAKAO_CLIENT_ID: string;
   // 다른 환경 변수를 추가적으로 정의
 }
 
