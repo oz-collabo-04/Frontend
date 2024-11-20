8a8003771e6b3ea0ca02c5546739a9f179c0920e
kimdaesik <ds500033@naver.com>
2024-11-20 14:06:44 +0900

    Fix: 충돌해결

--- src/uiComponents/ChatPage/ChatRoom.tsx
+++ src/uiComponents/ChatPage/ChatRoom.tsx
@@ -1,10 +1,6 @@
 import InputField from './InputField';
 import { useEffect, useState } from 'react';
-<<<<<<< Updated upstream
 // import socket from '@/config/server';
-=======
-import socket from '@/config/server';
->>>>>>> Stashed changes
 import ChatContainer from './ChatContainer';
 
 interface Message {
@@ -18,7 +14,6 @@ const ChatRoom = () => {
   const [messageList, setMessageList] = useState<Message[]>([]);
   console.log('messageList', messageList);
 
-<<<<<<< Updated upstream
   // // 웹소켓 연결
   // useEffect(() => {
   //   console.log('Socket instance:', socket);
@@ -84,73 +79,6 @@ const ChatRoom = () => {
   //     console.log('sendMessage res', res);
   //   });
   // };
-=======
-  // 웹소켓 연결
-  useEffect(() => {
-    console.log('Socket instance:', socket);
-    // socket 객체가 제대로 초기화되었는지 확인하기 위해 출력
-
-    socket.on('connect', () => {
-      console.log('Connected to server with id:', socket.id);
-    });
-    // 서버와 연결이 성공했을 때 실행되는 콜백 함수 등록
-    // 서버로부터 할당받은 socket ID를 출력
-
-    socket.on('response', (msg) => {
-      console.log('Message from server:', msg);
-    });
-    // 서버에서 "response"라는 이벤트가 발생했을 때 실행되는 콜백 함수 등록
-    // 서버에서 전송된 메시지를 콘솔에 출력
-
-    socket.on('disconnect', () => {
-      console.log('Disconnected from server');
-    });
-    // 서버와의 연결이 끊어졌을 때 실행되는 콜백 함수 등록
-    // 연결 해제 메시지를 콘솔에 출력
-
-    return () => {
-      socket.off('connect');
-      // "connect" 이벤트에 대한 리스너를 제거
-
-      socket.off('response');
-      // "response" 이벤트에 대한 리스너를 제거
-
-      socket.off('disconnect');
-      // "disconnect" 이벤트에 대한 리스너를 제거
-    };
-    // 컴포넌트가 언마운트되거나 리렌더링될 때 리스너 정리
-    // 메모리 누수 및 중복 이벤트 등록 방지
-  }, []);
-
-  // 유저 이름을 입력받고 로그인 처리
-  const askUserName = () => {
-    const userName = prompt('당신의 이름을 입력하세요.');
-    console.log('userName', userName);
-
-    socket.emit('login', userName, (res: { ok: boolean; data: { name: string } }) => {
-      if (res?.ok) {
-        setUser(res.data);
-      }
-    });
-  };
-
-  // 서버에서 메시지 수신 시 처리
-  useEffect(() => {
-    socket.on('message', (message) => {
-      setMessageList((prevState) => prevState.concat(message));
-    });
-
-    askUserName();
-  }, []);
-
-  // 메시지 전송 함수
-  const sendMessage = (event: React.FormEvent) => {
-    event.preventDefault();
-    socket.emit('sendMessage', message, (res: { ok: boolean }) => {
-      console.log('sendMessage res', res);
-    });
-  };
->>>>>>> Stashed changes
 
   return (
     <div className='chatRoom'>
