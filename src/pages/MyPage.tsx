731daae15da32b1d1922239cd3e10057ec513202
siltea <siltea2019@gmail.com>
2024-11-21 20:56:40 +0900

    Design : 유저리뷰페이지 중간저장

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -5,15 +5,13 @@ import { ReservationSection } from '@/uiComponents/MyPage/ReservationSection';
 import { useState } from 'react';
 
 export default function Mypage() {
-  const [isExpert, setIsExpert] = useState(false); // 임시코드. 전문가 여부에 따라 수정 예정
+  const [isExpert, setIsExpert] = useState(ture); // 임시코드. 전문가 여부에 따라 수정 예정
 
   return (
     <div className='myPage contentLayout'>
       <PageTitle title='마이페이지' isPrevBtn={true} prevUrl='/' />
-
       <main className='myMain'>
         <SmallTitle title='예약 관리' />
-
         <div className='reservationContainer'>
           <ReservationSection className='myReservation' title='예약 내역' linkTo='/reservation' />
           {isExpert && <ReservationSection className='myCalender' title='캘린더' linkTo='/calender' />}
---
+++ src/uiComponents/MyPage/ReservationList/UserReview.tsx
@@ -0,0 +1,5 @@
+const UserReview = () => {
+  return <div>UserReview</div>;
+};
+
+export default UserReview;
