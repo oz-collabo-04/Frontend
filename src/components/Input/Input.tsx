d8ac14359d59f4b46d8f402c9d3f1b21e5ee89f1
kimdaesik <ds500033@naver.com>
2024-11-27 17:17:08 +0900

    Chore: 빌드용 채팅페이지 코드 수정

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,5 @@
+import React, { ForwardedRef, forwardRef } from 'react';
+
 interface InputProps {
   type: 'text' | 'password';
   width?: string;
@@ -6,20 +8,25 @@ interface InputProps {
   placeholder?: string;
   disabled?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  // ref를 ForwardedRef로 타입 지정
+  ref?: ForwardedRef<HTMLInputElement>;
 }
 
-const Input = ({ type, width, height, value, placeholder, disabled, onChange }: InputProps) => {
-  return (
-    <input
-      type={type}
-      className='comInput'
-      placeholder={placeholder}
-      style={{ width, height }}
-      value={value}
-      disabled={disabled}
-      onChange={onChange}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type, width, height, value, placeholder, disabled, onChange }, ref) => {
+    return (
+      <input
+        type={type}
+        className='comInput'
+        placeholder={placeholder}
+        style={{ width, height }}
+        value={value}
+        disabled={disabled}
+        onChange={onChange}
+        ref={ref} // ref를 input 요소에 연결
+      />
+    );
+  }
+);
 
 export default Input;
