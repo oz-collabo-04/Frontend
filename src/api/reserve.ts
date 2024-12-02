9fbe114211213b83fbcaa78cf6e08f8f46555c12
siltea <siltea2019@gmail.com>
2024-12-02 12:06:36 +0900

    Design : Header 및 UserEstimationPage 레이아웃 변경

--- src/api/reserve.ts
+++ src/api/reserve.ts
@@ -1,10 +1,8 @@
-import { workerStartPromise } from '@/mocks/browsers';
-import axios from 'axios';
+import { client } from './axiosInstance';
 
 export const fetchReserveList = async () => {
-  await workerStartPromise;
   try {
-    const response = await axios.get('/mock/reservations');
+    const response = await client.get('/reservations/');
     console.log('response.status:', response.status);
     return response.data;
   } catch (err) {
