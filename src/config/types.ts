f5fac254447dc63b2a80860d65c7400d96101576
siltea <siltea2019@gmail.com>
2024-12-11 02:54:57 +0900

    Refactor : confirmPage API호출 및 기능 일부 수정

--- src/config/types.ts
+++ src/config/types.ts
@@ -33,6 +33,7 @@ export interface IReservationContentProps {
   title: string; // 서비스명
   reserveStatus: string; // 예약상태
   expertUser: string; // 전문가 이름
+  expertUserId: number; // 전문가 ID
   requestUser: string; // 요청자 이름
   charge: number; // 금액
   serviceTime: string; // 서비스 시간
@@ -120,4 +121,60 @@ export interface ILocation {
 }
 export interface ILocationObject {
   [key: string]: string;
+}
+
+// WeddingConfirm 데이터 타입
+export interface WeddingConfirm {
+  id: number;
+  user: number;
+  service_list: string[];
+  service_list_display: string;
+  prefer_gender: string;
+  prefer_gender_display: string;
+  created_at: string; // ISO8601 형식
+  updated_at: string; // ISO8601 형식
+  location: string;
+  location_display: string;
+  is_reception: boolean;
+  wedding_datetime: string; // ISO8601 형식
+  wedding_hall: string;
+  status: string;
+  status_display: string;
+}
+
+// ConfirmData 내부의 Estimation 타입
+export interface Estimation {
+  id: number;
+  charge: number;
+  created_at: string; // ISO8601 형식
+  due_date: string; // ISO8601 형식
+  location: string;
+  request: number;
+  request_user: {
+    id: number;
+    name: string;
+    email: string;
+    phone_number: string;
+  };
+  expert: {
+    id: number;
+    expert_image: string;
+    user: {
+      id: number;
+      name: string;
+      email: string;
+      phone_number: string;
+    };
+  };
+  service: string;
+}
+
+// ConfirmData 타입
+export interface ConfirmData {
+  id: number;
+  status: string;
+  created_at: string; // ISO8601 형식
+  updated_at: string; // ISO8601 형식
+  chatroom_id: number;
+  estimation: Estimation;
 }
--- src/uiComponents/MyPage/Reservation/EstimationConfirm/EstimationConfirm.tsx
+++ src/uiComponents/MyPage/Reservation/EstimationConfirm/EstimationConfirm.tsx
@@ -1,22 +1,69 @@
+import { auth } from '@/api/axiosInstance';
+import { fetchFinalConfirmData } from '@/api/reserve';
+import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
+import { ConfirmData, WeddingConfirm } from '@/config/types';
+import { formatDate } from '@/utils/formatDate';
+import { getServiceKorean } from '@/utils/serviceKorean';
+import { getStatusKorean } from '@/utils/statusKorean';
+import { useEffect, useState } from 'react';
+
 interface EstimationConfirmProps {
   estimationId: number; // 예상되는 타입 정의
-  charge: number;
+  expertUserId: number;
 }
 
 // estimation 타입지정 필요함
-const EstimationConfirm = ({ estimationId, charge }: EstimationConfirmProps) => {
+const EstimationConfirm = ({ estimationId, expertUserId }: EstimationConfirmProps) => {
+  const [confirmData, setConfirmData] = useState<ConfirmData | null>(null);
+  const [weddingData, setWeddingData] = useState<WeddingConfirm | null>(null);
+
+  // 웨딩홀 정보 호출 API요청
+  const fetchWeddingData = async (id: number): Promise<WeddingConfirm | undefined> => {
+    try {
+      const response = await auth.get(`/estimations/request/${id}`);
+      console.log('wedding hall data :', response.status);
+      return response.data as WeddingConfirm;
+    } catch (err) {
+      console.error('Error Fetching Wedding Data :', err);
+    }
+  };
+
+  // 예약 완료상태 api
+  const handleCompleteClick = () => {};
+
+  useEffect(() => {
+    const fetchEstimationData = async () => {
+      try {
+        const finalConfirmData = await fetchFinalConfirmData(estimationId);
+        const weddingConfirmData = await fetchWeddingData(expertUserId);
+        setConfirmData(finalConfirmData as ConfirmData); // 상태 업데이트
+        setWeddingData(weddingConfirmData || null);
+
+        console.log('weddingConfirm data:', weddingConfirmData); // 디버깅용
+        console.log('confirm data:', finalConfirmData); // 디버깅용
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchEstimationData(); // 비동기 함수 호출
+  }, [estimationId, expertUserId]);
+
+  if (!confirmData && !weddingData) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <>
-      <div className={`${estimationId}`}>EstimationConfirm</div>
-      <div>예약일자: </div>
-      <div>예약상태: {estimationId}</div>
-      <div>예약금액: {charge}</div>
-      <div>지역: </div>
-      <div>서비스 제공일: </div>
-      {/* <div>서비스 제공자: {data?.expert.user.name}</div> */}
-      <div>서비스 제공영역: </div>
-      {/* <div>서비스 제공자 연락처: {data?.expert.user.phone_number}</div>
-      <div>서비스 제공자 이미지: {data?.expert.expert_image}</div> */}
+      <div>예약일자: {confirmData && formatDate(confirmData.estimation.created_at)}</div>
+      <div>예약상태: {confirmData && getStatusKorean(confirmData.status)}</div>
+      <div>예약금액: {confirmData?.estimation.charge}</div>
+      <div>지역: {weddingData?.location_display}</div>
+      <div>서비스 제공일시: {weddingData && formatDate(weddingData.wedding_datetime)}</div>
+      <div>서비스 제공자: {confirmData?.estimation.expert.user.name}</div>
+      <div>서비스 제공영역: {confirmData && getServiceKorean(confirmData.estimation.service)}</div>
+      <div>서비스 제공자 연락처: {confirmData?.estimation.expert.user.phone_number}</div>
+      <div>서비스 제공자 이미지: {confirmData?.estimation.expert.expert_image}</div>
     </>
   );
 };
